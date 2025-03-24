dbcd175ca22c5cbb67ffc3eaebf9f74a75eb92d3
SHOUROV-GHARAMI <shourovgharami222@gmail.com>
2025-03-24 13:55:22 +0600

    30 days of code hacker rank solution

---
+++ hacker_rank_30days_code/hacher_rank_day1.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+x = int()
+y = float()
+z = str()
+# Read and save an integer, double, and String to your variables.
+x = int(input('Enter an integer: '))
+y = float(input('Enter a double: '))
+z = input('Enter a string: ')
+# Print the sum of both integer variables on a new line.
+print('\n', i + x)
+# Print the sum of the double variables on a new line.
+print('\n', d + y)
+# Concatenate and print the String variables on a new line
+print('\n', s + z)
+# The 's' variable above should be printed first.
